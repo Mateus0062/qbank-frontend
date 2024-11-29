473c883bddaf19b08c257be2e8a714839c21991d
Mateus0062 <mateusmendmattos@gmail.com>
2024-11-28 22:39:23 -0300

    att css

--- src/components/contas/Accounts.js
+++ src/components/contas/Accounts.js
@@ -1,8 +1,8 @@
 // src/components/Accounts.js
 import React, { useEffect, useState } from 'react';
-import api from '../../services/api';
-import './Accounts.css'
+import { Link } from 'react-router-dom'; // Importando Link do react-router-dom
 import apiProxy from '../../services/apiProxy';
+import './Accounts.css';
 
 const Accounts = () => {
   const [accounts, setAccounts] = useState([]);
@@ -25,11 +25,22 @@ const Accounts = () => {
     <div className='contas-container'>
       <h2 className='accounts'>Contas</h2>
       {error && <p>{error}</p>}
-      <ul>
+
+      <div className='accounts-list'>
         {accounts.map((account) => (
-          <li key={account.id}>{account.name} - {account.balance}</li>
+          <div key={account.id} className='account-card'>
+            <h3>Informações da Conta</h3>
+            <p><strong>Nome:</strong> {account.name}</p>
+            <p><strong>Idade:</strong> {account.age}</p>
+            <p><strong>Account Holder:</strong> {account.accountHolder}</p>
+            <p><strong>Account Number:</strong> {account.accountNumber}</p>
+            <p><strong>Balance:</strong> {account.balance}</p>
+          </div>
         ))}
-      </ul>
+      </div>
+
+      {/* Botão de Voltar para o Menu */}
+      <Link to="/" className="voltar-button">Voltar para o Menu</Link>
     </div>
   );
 };
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -26,8 +26,7 @@ const Login = () => {
   }
 
   return ( 
-     
-    <div className='login-container'>
+   <div className='login-container'>
       <img src={Logo}/>
       <h2>Login</h2>
       {error && <p>{error}</p>}
